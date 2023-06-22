b5199ec284a052b1cee427be2255e640bb7b8baa
CalebEng <121829627+CalebEng@users.noreply.github.com>
2023-06-22 14:31:23 -0400

    modified

--- temp.py
+++ temp.py
@@ -3,27 +3,15 @@
 #Messing with creating a discord chat bot
 
 
+import csv
 
-import glob
-import logging
-import os
-import pickle
-import random
-import re
-import shutil
-import json
-from typing import Dict, List, Tuple
-
-import numpy as np
-import pandas as pd
-
-from sklearn.model_selection import train_test_split
-
-from torch.nn.utils.rnn import pad_sequence
-from torch.utils.data import DataLoader, Dataset, RandomSampler, SequentialSampler
-from torch.utils.data.distributed import DistributedSampler
-from tqdm.notebook import tqdm, trange
-
+with open('Data set/conversation_enc.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count =0
+    for row in csv_reader:
+        if line_count ==0:
+            print(f"Column names are {', '.join(row)}")
+            line_count+=1
 
 
 
